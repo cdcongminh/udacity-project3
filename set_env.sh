d3100859767d4bd9c7822f084c9807b6cd5a8423
cdcongminh <cdcongminh@gmail.com>
2023-10-28 10:07:58 +0700

    upadte env

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=12345678
-export POSTGRES_HOST=minhnc14-udacity-db.chtknqldwn5k.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=minhnc14-udacity-database.chu6opo6aows.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::minhnc14-udacity-project
 export AWS_REGION=us-east-1
