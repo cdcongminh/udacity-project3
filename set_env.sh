13bd3ebe38a1e4e115bd611f206c07e760fc03cf
cdcongminh <cdcongminh@gmai.com>
2023-11-04 14:06:42 +0700

    update env

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=12345678
-export POSTGRES_HOST=udaproject3db.cw0pguwbtwak.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=udacity-project3-db.c7cdvsyqtxdt.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::udaproject3
-export AWS_REGION=us-east-1
+export AWS_BUCKET=arn:aws:s3:::udacity-project3-s3
+export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
